9d5d198e3ddfcbe914f4d308377daf055b59319a
Samuel Holmez <samuelholmez@Samuels-MacBook-Air.local>
2023-07-14 08:18:46 -0400

    ADDED Game Cards

---
+++ components/PageLayout/pageLayout.js
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function PageLayout({ children }){
+
+
+  return(
+      <>
+        <Header />
+          {children}
+        <Footer />
+      </>
+  )
+}
