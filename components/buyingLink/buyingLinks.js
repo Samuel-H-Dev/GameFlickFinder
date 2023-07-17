b5cb457265f12fa0fcc0b1d2bc449e5d11f4bc88
Samuel Holmez <samuelholmez@Samuels-MacBook-Air.local>
2023-07-17 09:57:51 -0400

    fixed error with links using :

--- components/buyingLink/buyingLinks.js
+++ components/buyingLink/buyingLinks.js
@@ -9,22 +9,22 @@ export default function Buyinglink({ game }) {
     <>
     
         {(game.PS)
-      ? <h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded"><a className="hover:underline" href={game.PS}>Download on Playstaion Here </a></h1>
+      ?  <a className="hover:underline" href={game.PS}><h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded">Download on Playstaion Here </h1></a>
       : <h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded"> Game is not avalible on Platform</h1>
         }
 
         {(game.XB)
-      ? <h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded"><a className="hover:underline" href={game.XB}>Download on X-Box Here </a></h1>
+      ? <a className="hover:underline" href={game.XB}><h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded">Download on X-Box Here </h1> </a>
       : <h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded"> Game is not avalible on X-Box</h1>
         }
 
         {(game.Switch)
-      ? <h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded"><a className="hover:underline" href={game.Switch}>Download on Switch Here </a></h1>
+      ? <a className="hover:underline" href={game.Switch}><h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded">Download on Switch Here </h1> </a>
       : <h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded"> Game is not avalible on Switch</h1>
         }
 
         {(game.PC)
-      ? <h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded"><a className="hover:underline" href={game.PC}>Download on PC Here </a></h1>
+      ? <a className="hover:underline" href={game.PC}><h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded">Download on PC Here </h1> </a>
       : <h1 className="px-8 hover:bg-slate-600 py-4 my-5 mx-3 bg-slate-700 rounded"> Game is not avalible on Platform</h1>
         }
 
